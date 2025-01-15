2dc9c23395b0c3f858f05bd52d5d5ef625669846
Terry Welsh <terry_welsh@anfcorp.com>
2025-01-15 12:13:07 -0500

    setup for new pattern

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 
-import { createPopper } from "@popperjs/core";
+import "@popperjs/core";
 import "bootstrap";
 
 import "./controllers";
-import * as bootstrap from "bootstrap";
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
 import "@hotwired/turbo-rails"
+import "@popperjs/core"
 import "popper"
 import "bootstrap"
 import "stripe"
 
 import "controllers"
-pin "bootstrap", to: "bootstrap.min.js"
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
